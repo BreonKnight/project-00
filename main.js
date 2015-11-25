32338ab26f73cd44973a11b8575fc6bb6f20ec86
Breon Knight <Breonknight7@gmail.com>
2015-11-25 14:00:22 -0800

    updated canvas and javascript functionality

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 console.log('Sanity Check: JS is working!');
 $(document).ready(function(){
 
-function Player(){
-	//cordinates on the game
-	this.xCord = xCord;
-	this.yCord = yCord;
 
-}
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
 
+	ctx.beginPath();
+	// x y width height
+	//x moves left and right
+	//y moves up and down 
+	//width of sqaure and height of it
+	ctx.rect(0,100,22,12);
+	//filling color
+	ctx.fillStyle = "blue";
+	ctx.fill();
+	ctx.closePath();
 
-
-
-
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
 
 });
