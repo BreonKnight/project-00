032f7f7f8ffa9198e5cce0a617518e3adbedf1f6
Breon Knight <Breonknight7@gmail.com>
2015-11-24 22:16:15 -0800

    added header / sticky footer and a canvas to draw on

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log('Sanity Check: JS is working!');
+$(document).ready(function(){
+
+function Player(){
+	//cordinates on the game
+	this.xCord = xCord;
+	this.yCord = yCord;
+
+}
+
+
+
+
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+});
