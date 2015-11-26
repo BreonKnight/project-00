2da3503263e634f88763a10e2126d8b16c4a956a
Breon Knight <Breonknight7@gmail.com>
2015-11-25 18:36:33 -0800

    edited functions in js file

--- main.js
+++ main.js
@@ -2,19 +2,38 @@ console.log('Sanity Check: JS is working!');
 $(document).ready(function(){
 
 
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
-	ctx.beginPath();
+var x = 0;
+var y = 400;
+var dx = 2;
+var dy = -2;
+
+
+function draw(){
+	//clears rectangle before each move
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	//drawng code
 	// x y width height
 	//x moves left and right
 	//y moves up and down 
 	//width of sqaure and height of it
-	ctx.rect(0,100,22,12);
+	ctx.beginPath();
+	ctx.rect(x,y,22,12);
 	//filling color
 	ctx.fillStyle = "blue";
 	ctx.fill();
-	ctx.closePath();
-
+	ctx.closePath(); 
+	x += dx;
+    y += dy;
+}
+setInterval(draw,10);
 
 });
+
+
+
+	// ctx.beginPath();
+
+
