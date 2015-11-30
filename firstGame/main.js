b70679e877ed023c1e36f9e909f930e03bd2b0db
Breon Knight <Breonknight7@gmail.com>
2015-11-30 07:14:29 -0800

    Reorganized

--- firstGame/main.js
+++ firstGame/main.js
@@ -6,9 +6,8 @@ $(document).ready(function(){
 	var ctx = canvas.getContext("2d");
 	var x = canvas.width/2;
 	var y = canvas.height /2;
-	// var bgImage = new Image();
-	// bgImage.src = 'forest.png'
-
+	var boxHeight = 10;
+	var boxWidth = 75;
 
 	//jQuery Button Reset
 	$('#restart').click(function clear(){
@@ -58,33 +57,32 @@ $(document).ready(function(){
 			players[i].wins = 0;
 		}
 		$("#p1").text("P1 wins: "+ players[0].wins);
-		$("#p2").text("P2 wins: " + players[1].wins);
+		$("#p2").text("P2 wins:" + players[1].wins);
 	}
 	
 
 	function detectWin(){
-		if(players[0].boxX > 600 - players[0].img.width) {
+		if(players[0].boxX > 600 - boxWidth) {
 			//alert("Player 1 wins");
 			resetPlayers();
 			players[0].wins++;
 			$("#p1").text("P1 wins: "+ players[0].wins);
 		}
-		if(players[1].boxX > 600 - players[1].img.width) {
+		if(players[1].boxX > 600 - boxWidth) {
 			//alert("Player 2 Wins");
 			resetPlayers();
 			players[1].wins++;
-			$("#p2").text("P2 wins: " + players[1].wins);
+			$("#p2").text("P2 wins:" + players[1].wins);
 		}
 	}
 
-	var Player = function(img, keycodeLeft, keycodeRight, boxY){
-		this.img = img;
+	var Player = function(color, keycodeLeft, keycodeRight){
+		this.color = color;
 		this.boxX = 0;
-		this.boxY = boxY;
+		this.boxY = canvas.height-boxHeight;
 		this.rightPressed = false;
 		this.leftPressed = false;
 		this.wins = 0;
-
 		var instance = this;
 		window.addEventListener("keydown",function(e){
 			if(e.keyCode == keycodeRight){
@@ -107,32 +105,32 @@ $(document).ready(function(){
 	Player.prototype.update = function() {
 		if(this.rightPressed) this.moveBy(7,0);
 		if(this.leftPressed) this.moveBy(-7,0);
-		this.drawSprite();
+		this.drawBox();
 	}
 
-	Player.prototype.drawSprite = function(){
-		ctx.drawImage(this.img, this.boxX, this.boxY, this.img.width, this.img.height);
-	}
-
-	function loadImage(img_url, cb){
-		var image = new Image();
-		image.src = img_url;
-		image.onload = function(){
-			console.log(image);
-			cb(image);
-		}
+	Player.prototype.drawBox = function(){
+		ctx.beginPath();
+		ctx.rect(this.boxX, this.boxY, boxWidth, boxHeight);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+		ctx.closePath();
 	}
 
 	Player.prototype.moveBy = function(moveX, moveY) {
 		this.boxX += moveX;
 		this.boxY += moveY;
-		if(this.boxX > canvas.width - this.img.width) {
-			this.boxX = canvas.width - this.img.width;
+		if(this.boxX > canvas.width-boxWidth) {
+			this.boxX = canvas.width-boxWidth;
 		}
 		if(this.boxX < 0) {
 			this.boxX = 0;
 		}
 	}
+
+	var players = [];
+
+	players.push(new Player("#00ff00", 65, 83));
+	players.push(new Player("#0000ff", 75, 76));
 
 	function draw(){
 		window.requestAnimationFrame(draw);
@@ -141,17 +139,12 @@ $(document).ready(function(){
 		finishLine();
 		for(var i = 0; i < players.length; i++) {
 			players[i].update();
+
 		}
-		detectWin();
+		detectWin()
 	}
 
-	var players = [];
-	loadImage("chrono.png", function(loaded_image){
-		players.push(new Player(loaded_image, 65, 83, 100));
-		loadImage("frog.png", function(loaded_frog_image){
-			players.push(new Player(loaded_frog_image, 75, 76, 300));
-			window.	requestAnimationFrame(draw);
-		});
-	});
+	// everything has been loaded
+	window.requestAnimationFrame(draw);
 
 });
--- secondGame/main.js
+++ secondGame/main.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
 	var ctx = canvas.getContext("2d");
 	var x = canvas.width/2;
 	var y = canvas.height /2;
-	var boxHeight = 10;
-	var boxWidth = 75;
-	var jumping = false;
+	// var bgImage = new Image();
+	// bgImage.src = 'forest.png'
+
 
 	//jQuery Button Reset
 	$('#restart').click(function clear(){
@@ -63,13 +63,13 @@ $(document).ready(function(){
 	
 
 	function detectWin(){
-		if(players[0].boxX > 600 - boxWidth) {
+		if(players[0].boxX > 600 - players[0].img.width) {
 			//alert("Player 1 wins");
 			resetPlayers();
 			players[0].wins++;
 			$("#p1").text("P1 wins: "+ players[0].wins);
 		}
-		if(players[1].boxX > 600 - boxWidth) {
+		if(players[1].boxX > 600 - players[1].img.width) {
 			//alert("Player 2 Wins");
 			resetPlayers();
 			players[1].wins++;
@@ -77,13 +77,14 @@ $(document).ready(function(){
 		}
 	}
 
-	var Player = function(color, keycodeLeft, keycodeRight){
-		this.color = color;
+	var Player = function(img, keycodeLeft, keycodeRight, boxY){
+		this.img = img;
 		this.boxX = 0;
-		this.boxY = canvas.height-boxHeight;
+		this.boxY = boxY;
 		this.rightPressed = false;
 		this.leftPressed = false;
 		this.wins = 0;
+
 		var instance = this;
 		window.addEventListener("keydown",function(e){
 			if(e.keyCode == keycodeRight){
@@ -106,32 +107,32 @@ $(document).ready(function(){
 	Player.prototype.update = function() {
 		if(this.rightPressed) this.moveBy(7,0);
 		if(this.leftPressed) this.moveBy(-7,0);
-		this.drawBox();
+		this.drawSprite();
 	}
 
-	Player.prototype.drawBox = function(){
-		ctx.beginPath();
-		ctx.rect(this.boxX, this.boxY, boxWidth, boxHeight);
-		ctx.fillStyle = this.color;
-		ctx.fill();
-		ctx.closePath();
+	Player.prototype.drawSprite = function(){
+		ctx.drawImage(this.img, this.boxX, this.boxY, this.img.width, this.img.height);
+	}
+
+	function loadImage(img_url, cb){
+		var image = new Image();
+		image.src = img_url;
+		image.onload = function(){
+			console.log(image);
+			cb(image);
+		}
 	}
 
 	Player.prototype.moveBy = function(moveX, moveY) {
 		this.boxX += moveX;
 		this.boxY += moveY;
-		if(this.boxX > canvas.width-boxWidth) {
-			this.boxX = canvas.width-boxWidth;
+		if(this.boxX > canvas.width - this.img.width) {
+			this.boxX = canvas.width - this.img.width;
 		}
 		if(this.boxX < 0) {
 			this.boxX = 0;
 		}
 	}
-
-	var players = [];
-
-	players.push(new Player("#00ff00", 65, 83));
-	players.push(new Player("#0000ff", 75, 76));
 
 	function draw(){
 		window.requestAnimationFrame(draw);
@@ -140,12 +141,17 @@ $(document).ready(function(){
 		finishLine();
 		for(var i = 0; i < players.length; i++) {
 			players[i].update();
-
 		}
-		detectWin()
+		detectWin();
 	}
 
-	// everything has been loaded
-	window.requestAnimationFrame(draw);
+	var players = [];
+	loadImage("chrono.png", function(loaded_image){
+		players.push(new Player(loaded_image, 65, 83, 100));
+		loadImage("frog.png", function(loaded_frog_image){
+			players.push(new Player(loaded_frog_image, 75, 76, 300));
+			window.	requestAnimationFrame(draw);
+		});
+	});
 
 });
